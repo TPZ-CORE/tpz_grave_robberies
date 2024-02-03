4177b8709ced04a1c7684bdf8b9af0eacda5f740
TPZ-CORE <152554963+TPZ-CORE@users.noreply.github.com>
2024-02-03 13:38:49 +0200

    Update fxmanifest.lua

--- tpz_grave_robberies/fxmanifest.lua
+++ tpz_grave_robberies/fxmanifest.lua
@@ -1,9 +1,10 @@
 fx_version "adamant"
 games {"rdr3"}
 rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
-author 'Nosmakos'
+
 version '1.0.0'
 
+author 'Nosmakos'
 description 'TPZ-CORE Grave Robberies'
 
 shared_scripts { 'config.lua', 'config_grave_locations.lua', 'locales.lua' }
